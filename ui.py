9e07fc89d6f7a2637a75e99ffa13d72581dd1784
developer1 <1clionelove@naver.com>
2023-04-04 14:15:47 +0900

    Modift activateMessage function to add an argument

--- ui.py
+++ ui.py
@@ -15,7 +15,7 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
-        self.btn1=QPushButton('Message',self)
+        self.btn1 = QPushButton('Message',self)
         self.btn2=QPushButton('Clear',self)
         
         self.le1=QLineEdit('0',self)
@@ -50,8 +50,8 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
+    def activateMessage(self, text):
+        self.te1.appendPlainText(text)
         
     def clearMessage(self):
         self.te1.clear()
